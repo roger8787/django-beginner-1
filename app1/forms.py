4293337a1c5266b2afd08d5fad7861a95933ebdb
Gomez Rogelio (Nokia) <gomez.rogelio@nokia.com>
2024-03-12 19:25:36 -0600

    adding style

--- app1/forms.py
+++ app1/forms.py
@@ -7,9 +7,9 @@ class ReservationForm(forms.ModelForm):
         exclude='name','num_tickets','num_will_assist','is_reserving_hotel','child_tickets','adults_tickets'
         
         widgets={
-            'reservation_code':forms.TextInput(attrs={'placeholder':'Enter reservation code', 'class':'form-control bg-secondary border-0 py-4 px-3'}), 
+            'reservation_code':forms.TextInput(attrs={'placeholder':'Ingresa tu codigo de asistencia', 'class':'form-control bg-secondary border-0 py-4 px-3'}), 
             'message':forms.Textarea(attrs={'class':'form-control bg-secondary border-0 py-2 px-3','placeholder':'Dejale un mensaje a los novios','rows':5}), 
-            'will_assist':forms.Select(choices=((True,'Si'), (False,'No')), attrs={'class':'form-control bg-secondary border-0'}),
+            'will_assist':forms.Select(choices=((True,'Si'), (False,'No')), attrs={'class':'form-control bg-secondary border-0 py-2 px-3'}),
             'mail':forms.EmailInput(attrs={'class':'form-control bg-secondary border-0 py-4 px-3','placeholder':'Ingresa tu email'})
         }
         
@@ -20,7 +20,7 @@ class NumberReservation(forms.ModelForm):
         model=Reservation
         fields = ['num_will_assist','is_reserving_hotel']
         widgets={
-            'num_will_assist':forms.NumberInput(attrs={'class':'form-control bg-secondary border-0 py-4 px-3', 'placeholder':'Ingresa cantidad de asistencias'}),
+            'num_will_assist':forms.NumberInput(attrs={'class':'form-control bg-secondary border-0 ', 'placeholder':'Ingresa cantidad de asistencias'}),
              'is_reserving_hotel':forms.Select(choices=((True,'Si'), (False,'No')), attrs={'class':'form-control bg-secondary border-0'}),
         }
         
